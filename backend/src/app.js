8659252fddffd3d3f4d38bcc4f6a748500c4a19d
vikas <vikas80046@gmail.com>
2024-01-25 20:14:34 +0530

    made some changes in vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,16 @@
 import express from "express";
 const app = express();
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import adminRouter from "./routers/adminRouter.js";
 import addResultRouter from "./routers/addResultRouter.js";
 import showResultRouter from "./routers/showResultRouter.js";
 import messageRouter from "./routers/message.router.js";
-app.use(express.json({
-    limit:"200kb"
-}
-));
+app.use(
+  express.json({
+    limit: "200kb",
+  })
+);
 app.use(
   express.urlencoded({
     extended: true,
@@ -24,11 +25,11 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieParser());
 
-app.use("/api/v1",adminRouter)
-app.use("/api/v1",addResultRouter)
-app.use("/api/v1",showResultRouter)
-app.use("/api/v1",messageRouter)
-app.use("/",(req,res)=>{
-  res.send("hello from server")
-})
+app.use("/api/v1", adminRouter);
+app.use("/api/v1", addResultRouter);
+app.use("/api/v1", showResultRouter);
+app.use("/api/v1", messageRouter);
+app.use("/", (req, res) => {
+  res.send("hello from server");
+});
 export default app;
