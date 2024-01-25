c34c64975556d924192c3e5595bb8fc755d798b7
vikas8004 <88913269+vikas8004@users.noreply.github.com>
2024-01-25 22:00:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:1200'
+      '/api':'https://rpaic-backend.vercel.app'
     }
   },
   plugins: [react()],
