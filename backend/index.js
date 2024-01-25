706b520d4c172cbbdc4498b9b3c03c13f6b38b95
vikas <vikas80046@gmail.com>
2024-01-25 23:17:06 +0530

    some changes made

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import app from "./src/app.js";
 import connect from "./src/db/conn.js";
 
 connect()
-  .then((_) => {
+  .then((res) => {
     const port = process.env.PORT || 1200;
     app.listen(port, () => {
       console.log(`Server is running on port number ${port}`);
